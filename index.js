0d4be5e11459c0c5c78b79ecac8d1f032423fb7b
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-05-20 23:52:13 -0400

    Make logged file path relative

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var fs = require('fs')
+  , path = require('path')
 
-module.exports = function (path, opts) {
-  if (!path) throw new Error('Path must be provided')
+module.exports = function (filepath, opts) {
+  if (!filepath) throw new Error('Path must be provided')
 
   return function (err, src) {
     if (!opts) opts = {}
     if (err) throw err
 
-    fs.writeFileSync(path, src, 'utf8');
+    fs.writeFileSync(filepath, src, 'utf8');
 
-    if (opts.debug) console.log('File written to ' + path)
+    if (opts.debug) console.log('File written to ' + path.relative(process.cwd(), filepath))
   }
 }
